c1763c2a015953765f304f037f438d12d4c0ce92
Rob Humphreys <rob@rpitv-sb-lm.(none)>
2013-08-04 03:31:52 -0400

    Delete *.d files

--- Debug/draw/BGRAn8_alpha_key.d
+++
@@ -1,6 +0,0 @@
-draw/BGRAn8_alpha_key.d: ../draw/BGRAn8_alpha_key.cpp \
- ../draw/../raw_frame.h ../draw/../types.h
-
-../draw/../raw_frame.h:
-
-../draw/../types.h:
--- Debug/draw/BGRAn8_blit.d
+++
@@ -1,6 +0,0 @@
-draw/BGRAn8_blit.d: ../draw/BGRAn8_blit.cpp ../draw/../raw_frame.h \
- ../draw/../types.h
-
-../draw/../raw_frame.h:
-
-../draw/../types.h:
